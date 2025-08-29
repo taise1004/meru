e1e0218c120aea5f5f614d0d3598518b282f3268
taise1004 <taise.1004.kuroki@gmail.com>
2025-08-29 18:50:47 +0000

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
+
+  validates :name, presence: { value: true, message: "名前を入力してください" },
+                   length: { maximum: 50, message: "名前は50文字以内で入力してください" }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true
+  validates :email, presence: { value: true, message: "メールアドレスを入力してください" },
+                    length: { maximum: 255, message: "メールアドレスは255文字以内です" },
+                    format: { with: VALID_EMAIL_REGEX, message: "メールアドレスの形式が正しくありません" },
+                    uniqueness: { message: "このメールアドレスはすでに登録されています" }
+
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: { value: true, message: "パスワードを入力してください" },
+                       length: { minimum: 6, message: "パスワードは6文字以上で入力してください" }
 end
 
