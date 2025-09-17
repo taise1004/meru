cf6657a33572e5bd30f0ce0c2fc92ef281e1bb74
taise1004 <taise.1004.kuroki@gmail.com>
2025-09-17 21:47:31 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :correct_user, only:[:edit,:update]
+  before_action :admin_user,     only: :destroy
 
+  def index
+    @users = User.paginate(page: params[:page])
+  end
   def show
     @user = User.find(params[:id])
   end
@@ -9,23 +15,59 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
     if @user.save
       reset_session
       log_in @user
-      flash[:success] = "ようこそ、#{@user.name}さん!"
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "編集に成功しました"
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url, status: :see_other
+  end
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
-  end
-end
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+    # beforeフィルタ
 
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url, status: :see_other) unless current_user?(@user)
+    end
+    def admin_user
+      redirect_to(root_url, status: :see_other) unless current_user.admin?
+    end
+end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,7 @@ module SessionsHelper
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
+    session[:session_token] = user.session_token
   end
 
     # 永続的セッションのためにユーザーをデータベースに記憶する
@@ -16,7 +17,10 @@ module SessionsHelper
   # 現在ログイン中のユーザーを返す（いる場合）
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
     elsif (user_id = cookies.encrypted[:user_id])
       user=User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
@@ -25,6 +29,11 @@ module SessionsHelper
       end
     end
   end
+  # 渡されたユーザーがカレントユーザーであればtrueを返す
+  def current_user?(user)
+    user && user == current_user
+  end
+
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
@@ -41,5 +50,9 @@ module SessionsHelper
     forget(current_user)
     reset_session
     @current_user = nil   # 安全のため
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
